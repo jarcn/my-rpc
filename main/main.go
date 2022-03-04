87aefbe33efaa12d80327edd950a1f1e1c1d9a49
chenjia <chenjia@qiyee-tech.com>
2022-03-04 23:18:02 +0800

    update

--- main/main.go
+++ main/main.go
@@ -12,6 +12,10 @@ import (
 )
 
 func startServer(addr chan string) {
+	var foo Foo
+	if err := myrpc.Register(&foo); err != nil {
+		log.Fatal("register error:", err)
+	}
 	l, err := net.Listen("tcp", ":0")
 	if err != nil {
 		log.Fatal("network error:", err)
@@ -48,7 +52,7 @@ func main_1() {
 	}
 }
 
-func main() {
+func main_2() {
 	log.SetFlags(0)
 	addr := make(chan string)
 	go startServer(addr)
@@ -67,6 +71,48 @@ func main() {
 				log.Fatal("call Foo.Sum error:", err)
 			}
 			log.Println("reply:", reply)
+		}(i)
+	}
+	wg.Wait()
+}
+
+type Foo int
+type Args struct{ Num1, Num2 int }
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
+func (f Foo) Square(args Args, reply *int) error {
+	*reply = args.Num1 * args.Num2
+	return nil
+}
+
+func (f Foo) SquareT(str1, str2 string, reply *string) error {
+	*reply = str1 + str2
+	return nil
+}
+
+func main() {
+	log.SetFlags(0)
+	addr := make(chan string)
+	go startServer(addr)
+	client, _ := myrpc.Dial("tcp", <-addr) //对go的net做了一次封装
+	defer func() { _ = client.Close() }()
+	time.Sleep(time.Second)
+	// send request & receive response
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := &Args{Num1: i, Num2: i}
+			var reply int
+			if err := client.Call("Foo.Square", args, &reply); err != nil {
+				log.Fatal("call Foo.Square error:", err)
+			}
+			log.Printf("%d * %d = %d", args.Num1, args.Num2, reply)
 		}(i)
 	}
 	wg.Wait()
--- server.go
+++ server.go
@@ -2,12 +2,13 @@ package myrpc
 
 import (
 	"encoding/json"
-	"fmt"
+	"errors"
 	"io"
 	"log"
 	"my-rpc/codec"
 	"net"
 	"reflect"
+	"strings"
 	"sync"
 )
 
@@ -24,7 +25,43 @@ var DefaultOption = &Option{
 	CodecType:   codec.GobType,
 }
 
-type Server struct{}
+type Server struct {
+	serviceMap sync.Map
+}
+
+//Register publishes in the server the set of methods of the
+func (server *Server) Register(rcvr interface{}) error {
+	s := newService(rcvr)
+	if _, dup := server.serviceMap.LoadOrStore(s.name, s); dup {
+		return errors.New("rpc# service already defined: " + s.name)
+	}
+	return nil
+}
+
+//Register publishes the receiver's methods in the DefaultServer.
+func Register(rcvr interface{}) error {
+	return DefaultServer.Register(rcvr)
+}
+
+func (server *Server) findService(serviceMethod string) (svc *service, mType *methodType, err error) {
+	dot := strings.LastIndex(serviceMethod, ".")
+	if dot < 0 {
+		err = errors.New("rpc server# service/method request ill-formed: " + serviceMethod)
+		return
+	}
+	serviceName, methodName := serviceMethod[:dot], serviceMethod[dot+1:]
+	svci, ok := server.serviceMap.Load(serviceName)
+	if !ok {
+		err = errors.New("rpc server# can't find service " + serviceName)
+		return
+	}
+	svc = svci.(*service)
+	mType = svc.method[methodName]
+	if mType == nil {
+		err = errors.New("rpc server# can't find method " + methodName)
+	}
+	return
+}
 
 func NewServer() *Server {
 	return &Server{}
@@ -93,6 +130,8 @@ func (server *Server) serveCodec(cc codec.Codec) {
 type request struct {
 	h            *codec.Header
 	argv, replyv reflect.Value
+	mtype        *methodType
+	svc          *service
 }
 
 func (server *Server) readRequestHeader(cc codec.Codec) (*codec.Header, error) {
@@ -112,11 +151,34 @@ func (server *Server) readRequest(cc codec.Codec) (*request, error) {
 		return nil, err
 	}
 	req := &request{h: h}
-	req.argv = reflect.New(reflect.TypeOf(""))
-	if err = cc.ReadBody(req.argv.Interface()); err != nil {
+	req.svc, req.mtype, err = server.findService(h.ServiceMethod)
+	if err != nil {
+		return req, err
+	}
+	req.argv = req.mtype.newArgv()
+	req.replyv = req.mtype.newReplyv()
+
+	argvi := req.argv.Interface()
+	if req.argv.Type().Kind() != reflect.Ptr {
+		argvi = req.argv.Addr().Interface()
+	}
+	if err = cc.ReadBody(argvi); err != nil {
 		log.Println("rpc server# read argv err:", err)
+		return req, err
 	}
 	return req, nil
+}
+
+func (server *Server) handleRequest(cc codec.Codec, req *request, sending *sync.Mutex, wg *sync.WaitGroup) {
+	defer wg.Done()
+	err := req.svc.call(req.mtype, req.argv, req.replyv)
+	if err != nil {
+		req.h.Error = err.Error()
+		server.sendResponse(cc, req.h, invalidRequest, sending)
+	}
+	// log.Println(req.h, req.argv.Elem())
+	// req.replyv = reflect.ValueOf(fmt.Sprintf("geerpc resp %d", req.h.Seq)) //真正处理请求的逻辑
+	server.sendResponse(cc, req.h, req.replyv.Interface(), sending)
 }
 
 func (server *Server) sendResponse(cc codec.Codec, h *codec.Header, body interface{}, sending *sync.Mutex) {
@@ -125,11 +187,4 @@ func (server *Server) sendResponse(cc codec.Codec, h *codec.Header, body interfa
 	if err := cc.Write(h, body); err != nil {
 		log.Println("rpc server# write response error:", err)
 	}
-}
-
-func (server *Server) handleRequest(cc codec.Codec, req *request, sending *sync.Mutex, wg *sync.WaitGroup) {
-	defer wg.Done()
-	log.Println(req.h, req.argv.Elem())
-	req.replyv = reflect.ValueOf(fmt.Sprintf("geerpc resp %d", req.h.Seq)) //真正处理请求的逻辑
-	server.sendResponse(cc, req.h, req.replyv.Interface(), sending)
 }
